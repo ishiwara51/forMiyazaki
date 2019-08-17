2e627c42dbb10fcb5b6814a17b6f310bd1e6005e
DaichiKanoh <onionsouop227@gmail.com>
2019-08-18 02:44:44 +0900

    ffqfq

--- test.py
+++ test.py
@@ -58,7 +58,7 @@ def first_login():
                     + str(datetime.date.today())
                     + '\' as date), 0, cast(\''
                     + str(datetime.datetime.now())
-                    + '\' as datetime)')
+                    + '\' as datetime))')
     else:
         return 'Your device was not able to be certificated.'
 
