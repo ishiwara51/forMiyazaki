3c497f5f7806cf6844178b8cbb116b2f9a634476
DaichiKanoh <onionsouop227@gmail.com>
2019-08-21 16:19:50 +0900

    fwrwr

--- test.py
+++ test.py
@@ -88,11 +88,11 @@ def chorus_end():
 
 @app.route('/tutorial_end', methods=['POST'])
 def tutorial_end():
-    print(request.form.get('lesson_num'))
-    print(type(request.form.get('lesson_num')))
+    print(int(request.form.get('lesson_num')))
+    print(type(int(request.form.get('lesson_num'))))
     if request.form.get('user_id') and request.form.get('lesson_num'):
         stmt = 'update user_info set updated_at=cast(%s as datetime), lesson_completed=%d where user_id=%d'
-        param_placeholders = (str(datetime.datetime.now()), int.from_bytes(request.form.get('lesson_num')), int.from_bytes(request.form.get('user_id')))
+        param_placeholders = (str(datetime.datetime.now()), int(request.form.get('lesson_num')), int.(request.form.get('user_id')))
         """
         stmt = str('update user_info set updated_at=cast(\''
                     + str(datetime.datetime.now())
