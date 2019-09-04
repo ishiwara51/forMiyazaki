831d291095c1648d8be3b127ca103fb6bfa06397
DaichiKanoh <onionsouop227@gmail.com>
2019-09-04 21:47:43 +0900

    Created

--- test.py
+++ test.py
@@ -1,11 +1,11 @@
-#from magenta.models.improv_rnn import improv_rnn_generate as gen
+from magenta.models.improv_rnn import improv_rnn_generate as gen
 from flask import Flask, request
 import sys
 
 #https://it-engineer-lab.com/archives/1181#_INSERT
 
 app = Flask(__name__)
-"""
+
 gen.FLAGS.config = 'chord_pitches_improv'
 gen.FLAGS.bundle_file = 'chord_pitches_improv.mag'
 gen.output_dir = '~/test/tmp/generated'
@@ -13,10 +13,10 @@ gen.FLAGS.num_outputs = 1
 gen.FLAGS.primer_melody = "[60]"
 gen.FLAGS.backing_chords = 'Dmaj7'
 gen.FLAGS.qpm = 120
-"""
+
 @app.route('/generate', methods=['POST'])
 def generate():   
-    """print(request.form.get('primer_melody'))
+    print(request.form.get('primer_melody'))
     print(request.form.get('backing_chords'))
     print(request.form.get('qpm'))
 
@@ -30,9 +30,9 @@ def generate():
 
     gen.FLAGS.primer_melody = "[60]"
     gen.FLAGS.backing_chords = 'Dmaj7'
-    gen.FLAGS.qpm = 120"""
+    gen.FLAGS.qpm = 120
 
-    return ""
+    return return_str
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, threaded=True)    
